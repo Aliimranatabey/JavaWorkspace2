a5e6d31e2b4f6b9eae453e6df4543998e90e0f3d
Aliimranatabey <aliimranatabey@gmail.com>
2023-06-28 19:19:19 +0300

    autoboxing ve unboxing eklendi

--- AutoboxingVeUnboxing/src/Main.java
+++ AutoboxingVeUnboxing/src/Main.java
@@ -1,9 +1,38 @@
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		/*
+		 * 
+		 * WRAPPER CLASS
+		 * 
+		 * boolean ---------> Boolean char ------------> Character byte ------------>
+		 * Byte short -----------> Short int -------------> Integer long ------------>
+		 * Long float -----------> Float double ----------> Double
+		 * 
+		 */
+
+		ArrayList<String> arrayList = new ArrayList<String>();
+		// ArrayList<int> arrayList2=new ArrayList<int>(); // Bu arraylistler içerisine
+		// referans bir tip koymamız gerekiyor yani ArrayList<int> gibi ilkel bir veri
+		// tipi koyarsak hata alırız . ArrayList<Integer> şeklinde yazmamız gerekiyor .
+
+		ArrayList<Integer> arrayList2 = new ArrayList<Integer>();
+
+		for (int i = 0; i < 10; i++) {
+			arrayList2.add(Integer.valueOf(i * 4)); // AUTOBOXING : Biz primitive (ilkel) bir veri tipi olan i değerini
+													// bir kutuya alıyoruz ve bunu wrapper class cinsinde arrayliste
+													// atıyoruz .
+			arrayList2.add(i * 4); // JAVA BIZE KENDINI GEREKSIZ KASMA BU SEKILDE YAZ BIRAK DIYOR .
+		}
+		for (int i = 0; i < arrayList2.size(); i++) {
+			arrayList2.add(arrayList2.get(i).intValue()); // UNBOXING : Biz o kutudan o degeri cikariyoruz .
+			arrayList2.add(arrayList2.get(i)); // JAVA BIZE KENDINI GEREKSIZ KASMA BU SEKILDE YAZ BIRAK DIYOR .
+		}
+		// Ancak java bu autoboxing ve unboxing olaylarını programcıdan beklemiyor java
+		// kendisi otamatik olarak yapiyor .
 	}
 
 }
