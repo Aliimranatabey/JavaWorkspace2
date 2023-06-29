1ba0f70f69d9e22bf065742479c2a6018d947847
Aliimranatabey <aliimranatabey@gmail.com>
2023-06-29 17:35:26 +0300

    Linkedlist added

--- LinkedList1/src/Main.java
+++ LinkedList1/src/Main.java
@@ -3,20 +3,20 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		ArrayList<String> diller=new ArrayList<String>();
+
+		ArrayList<String> diller = new ArrayList<String>();
 		diller.add("Java");
 		diller.add("Php");
 		diller.add("Python");
 		diller.add("Kotlin");
 		diller.add("C#");
-		
-		for(String s:diller) {
+
+		for (String s : diller) {
 			System.out.println(s);
 		}
 		System.out.println("------------------------------");
 		diller.add(1, "C++");
-		for(String s:diller) {
+		for (String s : diller) {
 			System.out.println(s);
 		}
 
