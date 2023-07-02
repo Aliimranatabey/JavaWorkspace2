adace166e676c36ea33a73f15841439be809703e
Aliimranatabey <aliimranatabey@gmail.com>
2023-07-02 17:24:45 +0300

    Basit Matematik ve Fizik Problemleri (Inner Class Kullanim Ornegi) Guncellendi

--- MatematikVeFizikProblemleri/src/Main.java
+++ MatematikVeFizikProblemleri/src/Main.java
@@ -1,8 +1,49 @@
+import java.util.Scanner;
 
 public class Main {
 
+	// Basit Matematik ve Fizik Problemler
+	// Daire Alan - Matematik
+	// Ucgen Cevresi - Matematik
+	// 3 Boyutlu Vektorlerin Ic Carpimi - Fizik
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Matematik ve Fizik Problemleri Programina Hos Gelidiniz...");
+		String islemler = "Islemler...\n" + "1. Daire Alani Hesaplama\n" + "2. Ucgen Cevresi Hesaplama\n"
+				+ "3. Iki Vektorun Ic Carpimini Hesaplama\n" + "Cikis : q";
+
+		while (true) {
+			System.out.println(islemler);
+			System.out.print("Islemi seciniz : ");
+			String islem = scanner.nextLine();
+
+			if (islem.equals("q")) {
+				System.out.println("Programdan cikiliyor...");
+				break;
+			} else if (islem.equals("1")) {
+				System.out.print("Dairenin Yaricapi : ");
+				int yaricap = scanner.nextInt();
+				scanner.nextLine();
+				Problem.Matematik.daireAlan(yaricap);
+			} else if (islem.equals("2")) {
+				System.out.print("Kenar 1 : ");
+				int kenar1 = scanner.nextInt();
+				System.out.print("Kenar 2 : ");
+				int kenar2 = scanner.nextInt();
+				System.out.print("Kenar 3 : ");
+				int kenar3 = scanner.nextInt();
+				scanner.nextLine();
+				Problem.Matematik.ucgenCevresi(kenar1, kenar2, kenar3);
+			} else if (islem.equals("3")) {
+				Vec vec1 = new Vec("Vector 1");
+				Vec vec2 = new Vec("Vector 2");
+				Problem.Fizik.icCarpim(vec1, vec2);
+			} else {
+				System.out.println("Gecersiz Islem...");
+			}
+		}
 
 	}
 
--- MatematikVeFizikProblemleri/src/Vec.java
+++ MatematikVeFizikProblemleri/src/Vec.java
@@ -1,4 +1,59 @@
+import java.util.Scanner;
 
 public class Vec {
+
+	private String isim;
+	private int i;
+	private int j;
+	private int k;
+
+	public Vec(String isim) {
+
+		this.isim = isim;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Vektorun i , j ve k degerlerini girin");
+		System.out.print("i : ");
+		this.i = scanner.nextInt();
+		scanner.nextLine();
+		System.out.print("j : ");
+		this.j = scanner.nextInt();
+		scanner.nextLine();
+		System.out.print("k : ");
+		this.k = scanner.nextInt();
+		scanner.nextLine();
+
+	}
+
+	public String getIsim() {
+		return isim;
+	}
+
+	public void setIsim(String isim) {
+		this.isim = isim;
+	}
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+
+	public int getJ() {
+		return j;
+	}
+
+	public void setJ(int j) {
+		this.j = j;
+	}
+
+	public int getK() {
+		return k;
+	}
+
+	public void setK(int k) {
+		this.k = k;
+	}
 
 }
